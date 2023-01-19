c4a24bf1dfd084f687a9ffcba63d5a21facba5df
choc1403 <eloicx@gmail.com>
2023-01-18 18:10:54 -0600

    Aprendiendo a como se declaran las variables en go

--- BASICO/Leccion 01/src/main.go
+++ BASICO/Leccion 01/src/main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
+	// Primer hola mundo desde Go
+
 	fmt.Println("Hola Mundo :)")
+	fmt.Println()
+
+	// Declaracion de variables constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1416
+
+	fmt.Println("Declaracion de pi1: ", pi, "\nDeclaracion de pi2: ", pi2)
+	fmt.Println()
+
+	// Declaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+	area = base * altura
+
+	fmt.Println("La base: ", base, "\nLa altura: ", altura, "\nLa Area: ", area)
+	fmt.Println()
+
 }
