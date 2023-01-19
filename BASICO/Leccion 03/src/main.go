7c7e68600e3b895f06a5c316bd3958de3aee5519
choc1403 <eloicx@gmail.com>
2023-01-18 18:50:23 -0600

    Conociendo mas sobre fmt

--- BASICO/Leccion 03/src/main.go
+++ BASICO/Leccion 03/src/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
+	// Conociendo el paquete fmt.Print
+
+	hola := "Holaaaa"
+	hello := "Que pasoooo"
+
+	fmt.Println(hola, hello)
+
+	nombre := "Plazti"
+	cursos := 500
+
+	fmt.Printf("%s Tiene mas de %d cursos\n", nombre, cursos)
+	fmt.Printf("%v Tiene mas de %v cursos\n", nombre, cursos)
+
+	mensaje := fmt.Sprintf("%s Tiene mas de %d cursos", nombre, cursos)
+	fmt.Println(mensaje)
+
+	//Conocer el tipo de dato
+	fmt.Printf("Que tipo de dato es la variable nombre %T\n", nombre)
 
 }
