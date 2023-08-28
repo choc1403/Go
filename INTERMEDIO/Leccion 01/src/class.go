1f58a57bcc2f582cf7217935b3e9fe4250110051
choc1403 <eloicx@gmail.com>
2023-08-28 12:12:55 -0600

    Comprendiendo a como se crean los metodos en go

--- INTERMEDIO/Leccion 01/src/class.go
+++ INTERMEDIO/Leccion 01/src/class.go
@@ -16,11 +16,42 @@ type Employee struct {
 	name string
 }
 
+// Creando un "metodo" Set en Go
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+// Creando un "Metodo" Get en Go
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+func (e *Employee) GetName() string {
+	return e.name
+}
+
 func main() {
 	e := Employee{}
+	// Viendo la informacion de la variable que acabamos de crear
 	fmt.Printf("%v", e)
+	fmt.Println("")
+
+	// Asignando Valores
 	e.id = 1
 	e.name = " Juan X"
 	fmt.Printf("%v", e)
+	fmt.Println("")
+
+	// Aprobando la utilizacion de metodos en Go
+	e.SetId(5)
+	e.SetName("Name 2")
+	fmt.Printf("%v", e)
+	fmt.Println("\n-----------------------")
+	fmt.Println(e.GetId())
+	fmt.Println(e.GetName())
 
 }
