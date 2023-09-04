88f56b82a86c32db9417e7fbbe540d4089964838
choc1403 <eloicx@gmail.com>
2023-09-04 11:54:37 -0600

    Conociendo como identificar los metodos de HTTP en las cuales esta accediendo el usuario

--- go_web/Introduccion/main.go
+++ go_web/Introduccion/main.go
@@ -6,17 +6,36 @@ import (
 	"net/http"
 )
 
+/*
+	Metodos HTTP:
+	GET, POST, PUT, DELETE
+*/
+
 func main() {
 	// Creando rutas en go
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Nombre", "Valor de header") // se podra mostrar en curl -i localhost:3000
+		w.Header().Add("Nombre", "Valor de header") // se podra mostrar en curl -i https://localhost:3000
 		fmt.Fprintf(w, "Hola Mundo")
-		http.Redirect(w, r, "/docs", 301) // Redireccionamientos
+		fmt.Println("El metodo es +" + r.Method) //Conociendo el metodo efectuado
+		http.Redirect(w, r, "/docs", 301)        // Redireccionamientos
 		// 301 - http.StatusMovedPermanet - mejor colocar constantes
 	})
 
 	http.HandleFunc("/docs", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Estas en el apartado de documentaciones")
+		// Conciendo los metodos en las cuales esta accediendo el usuario
+		switch r.Method {
+		case "GET":
+			fmt.Fprintf(w, "Hola mundo desde el metodo GET")
+		case "POST":
+			fmt.Fprintf(w, "Hola mundo desde el metodo POST")
+		case "PUT":
+			fmt.Fprintf(w, "Hola mundo desde el metodo PUT")
+		case "DELETE":
+			fmt.Fprintf(w, "Hola mundo desde el metodo DELETE")
+		default:
+			http.Error(w, "Metodo no valido", http.StatusBadRequest)
+		}
 
 	})
 
