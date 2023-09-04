0d99121325b010861de821abb088131bdda0e9fe
choc1403 <eloicx@gmail.com>
2023-09-04 11:41:31 -0600

    paginas no encontradas

--- go_web/Introduccion/main.go
+++ go_web/Introduccion/main.go
@@ -11,12 +11,17 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Nombre", "Valor de header") // se podra mostrar en curl -i localhost:3000
 		fmt.Fprintf(w, "Hola Mundo")
-		http.Redirect(w, r, "/docs", 301)
+		http.Redirect(w, r, "/docs", 301) // Redireccionamientos
 
 	})
 
 	http.HandleFunc("/docs", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Estas en el apartado de documentaciones")
+
+	})
+
+	http.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
 	})
 
 	// Para levantar un servidor en go
