6175a7f15133750470d5c90ff9f4f17084cda727
choc1403 <eloicx@gmail.com>
2023-09-04 11:29:56 -0600

    Conociendo los handlefunc

--- go_web/Introduccion/main.go
+++ go_web/Introduccion/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hola Mundo")
+
+	})
+
+	// Para levantar un servidor en go
 	log.Fatal(http.ListenAndServe("localhost:3000", nil))
 }
